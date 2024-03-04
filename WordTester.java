da38785e61bb26b7fa9a6cb0c88aa12dabb8f855
fuzesmonk <ekehana209@gmail.com>
2024-03-04 10:39:27 -0800

    Added Requirements file and pulled word tester file

---
+++ WordTester.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+/**
+ * Partner Lab 3 Tester Program
+ * @author Hellen Pacheco (main method)
+ */
+
+public class WordTester {
+
+    /**
+     * Method to test the Antics methods
+     * @param args Command-line arguments (not used)
+     */
+
+    public static void main(String[] args) {
+        // Test words
+        String word;
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.print("Enter word (exit to end): ");
+            word = input.nextLine();
+            System.out.println("Word: " + word);
+            System.out.println("Is Palindrome: " + Antics.isPalindrome(word));
+            System.out.println("Is Abecedarian: " + Antics.isAbecedarian(word));
+            System.out.println("Capitalized: " + Antics.capitalizeFirstLetter(word));
+            System.out.println("--------------------------------------------------");
+        } while (!word.equalsIgnoreCase("exit"));
+        System.out.println("Goodbye");
+        input.close();
+    }
+}
