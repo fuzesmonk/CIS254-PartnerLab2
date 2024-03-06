3b65a078462704c793655eda5e6c58c7a34c7b32
fuzesmonk <ekehana209@gmail.com>
2024-03-05 23:49:57 -0800

    Starting on Palindrome Method, need to start testing

--- Antics.java
+++ Antics.java
@@ -1,9 +1,34 @@
-public class Antics {
+public class Antics extends WordTester{
+    //Getting length of word for use
+    WordTester wordTests = new WordTester();
+    String inputtedWord = wordTests.word;
+    int lengthOfWord = inputtedWord.length();
     
+    public boolean isPalindrome() {
+        boolean palindrome = true;
+        int rightLetter = lengthOfWord;
+        for(int leftLetter = 0; leftLetter < rightLetter;) {
+            if (inputtedWord.charAt(leftLetter) != inputtedWord.charAt(rightLetter)){
+                palindrome = false;
+            }
+            else{
+                rightLetter--;
+                leftLetter++;
+            }
+        }
+        return palindrome;
+            
+
+        }
+
+
+
+
+    }
+
     //Ella: Palindrome
     //Xinyi: Abecdarian
-    //YuWen Shirley : Capitalize first letter
+    //YuWen or Shirley : Capitalize first letter
 
 
-    
 }
--- WordTester.java
+++ WordTester.java
@@ -3,17 +3,15 @@ import java.util.Scanner;
  * Partner Lab 3 Tester Program
  * @author Hellen Pacheco (main method)
  */
-
 public class WordTester {
-
     /**
      * Method to test the Antics methods
      * @param args Command-line arguments (not used)
-     */
+     */]
 
     public static void main(String[] args) {
         // Test words
-        String word;
+        public String word;
         Scanner input = new Scanner(System.in);
         do {
             System.out.print("Enter word (exit to end): ");
